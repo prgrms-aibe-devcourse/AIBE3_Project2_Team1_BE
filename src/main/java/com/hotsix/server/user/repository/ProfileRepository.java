f5ca31a83fcdbe2d5297a212151724ca9f43efe3
seungin <tmddls91@inha.edu>
2025-09-28 03:03:13 +0900

    [fix] 유저 도메인 안에 있던 ProfileRepository 제거

--- src/main/java/com/hotsix/server/user/repository/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotsix.server.user.repository;
-
-import com.hotsix.server.profile.entity.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-}
