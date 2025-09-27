3e9f309527ad1ef0d607e65c4e5923b360e4e6e1
seungin <tmddls91@inha.edu>
2025-09-28 02:54:16 +0900

    [feat] 프로필 관련 레포지토리,서비스 초기 구성

---
+++ src/main/java/com/hotsix/server/profile/service/ProfileService.java
@@ -0,0 +1,9 @@
+package com.hotsix.server.profile.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+}
