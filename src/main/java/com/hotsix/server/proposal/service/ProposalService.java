836102f179d2f45185801a077b6219cad8590dad
seungin <tmddls91@inha.edu>
2025-09-28 03:41:55 +0900

    [feat] 제안/계약 도메인 관련 컨트롤러,레포지토리,서비스 로직 초기 구성

---
+++ src/main/java/com/hotsix/server/proposal/service/ProposalService.java
@@ -0,0 +1,10 @@
+package com.hotsix.server.proposal.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProposalService {
+    // 제안 제출, 수락/거절, 계약 생성 로직 관련
+}
