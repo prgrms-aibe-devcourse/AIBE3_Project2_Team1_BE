b40f583d619f1447086eb25cd816b775d0dfc3c2
seungin <tmddls91@inha.edu>
2025-09-28 03:18:06 +0900

    [feat] 프로젝트 도메인 관련 엔티티 초기 구성 (프로젝트,북마크)

---
+++ src/main/java/com/hotsix/server/project/entity/Bookmark.java
@@ -0,0 +1,27 @@
+package com.hotsix.server.project.entity;
+
+import com.hotsix.server.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import com.hotsix.server.user.entity.User;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "bookmarks")
+public class Bookmark extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookmarkId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+}
