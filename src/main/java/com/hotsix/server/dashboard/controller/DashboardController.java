a341fa67485a53419cd7f4865dc89cd6e7399c6c
seungin <tmddls91@inha.edu>
2025-09-29 01:11:44 +0900

    [feat] 대시보드 도메인 관련 파일 초기 구성

---
+++ src/main/java/com/hotsix/server/dashboard/controller/DashboardController.java
@@ -0,0 +1,12 @@
+package com.hotsix.server.dashboard.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+    // 대시 보드 조회 API 구현
+}
---
+++ src/main/java/com/hotsix/server/dashboard/dto/DashboardResponseDto.java
@@ -0,0 +1,4 @@
+package com.hotsix.server.dashboard.dto;
+
+public class DashboardResponseDto {
+}
