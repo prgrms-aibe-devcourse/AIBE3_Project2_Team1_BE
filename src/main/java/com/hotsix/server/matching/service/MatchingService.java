3f1b0984bde7ebd49617ed359b787ab549e2fcd8
seungin <tmddls91@inha.edu>
2025-09-29 01:07:57 +0900

    [feat] AI 기반 프로젝트 매칭 도메인 관련 초기 구성

---
+++ src/main/java/com/hotsix/server/matching/service/MatchingService.java
@@ -0,0 +1,11 @@
+package com.hotsix.server.matching.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MatchingService {
+    // GPT, GEMINI 의 외부 API를 연동하는것이기 때문에 엔티티와 레포지토리는 필요 X
+    // 외부 API 연동에 대한 로직을 구현해주세요. (필요하다면 본인이 dto 생성)
+}
