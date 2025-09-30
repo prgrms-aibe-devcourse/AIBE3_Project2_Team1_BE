2bca44bd99268da5b71949829947bddc607cd806
Jeongho Kim <kimjungho.kr@gmail.com>
2025-09-30 12:21:42 +0900

    [feat] 제안서, 계약 상태 enum 변경

--- src/main/java/com/hotsix/server/proposal/entity/ContractStatus.java
+++ src/main/java/com/hotsix/server/proposal/entity/ContractStatus.java
@@ -1,5 +1,6 @@
 package com.hotsix.server.proposal.entity;
 
 public enum ContractStatus {
-    CONTRACTED, COMPLETED, CANCELLED
+    DFAFT, REVIEW, COMPLETED, TERMINATED, CANCELLED
+    //초안, 검토중, 체결됨, 취소, 종료됨
 }
--- src/main/java/com/hotsix/server/proposal/entity/ProposalStatus.java
+++ src/main/java/com/hotsix/server/proposal/entity/ProposalStatus.java
@@ -1,5 +1,5 @@
 package com.hotsix.server.proposal.entity;
 
 public enum ProposalStatus {
-    SENT, ACCEPTED, REJECTED // 프로젝트 제안 상태 관련 (제안 보냄, 수락, 거절)
+    DRAFT, SUBMITTED, ACCEPTED, REJECTED, // 프로젝트 제안 상태 관련 (작성중, 제출됨, 수락됨, 거절됨)
 }
