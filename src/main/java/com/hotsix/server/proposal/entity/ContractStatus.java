71f19c395995be2d048bbb4ff99194a21dbe4cda
Jeongho Kim <kimjungho.kr@gmail.com>
2025-09-30 17:35:20 +0900

    [fix] Contract의 enum 상수명 수정

--- src/main/java/com/hotsix/server/proposal/entity/ContractStatus.java
+++ src/main/java/com/hotsix/server/proposal/entity/ContractStatus.java
@@ -1,6 +1,6 @@
 package com.hotsix.server.proposal.entity;
 
 public enum ContractStatus {
-    DFAFT, REVIEW, COMPLETED, TERMINATED, CANCELLED
+    DRAFT, REVIEW, COMPLETED, TERMINATED, CANCELLED
     //초안, 검토중, 체결됨, 취소, 종료됨
 }
