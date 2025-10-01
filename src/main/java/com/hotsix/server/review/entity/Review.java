78f50f4dc84c3f4aebe84efc9cba90542a29fe92
seungin <tmddls91@inha.edu>
2025-10-01 15:32:23 +0900

    [feat] 리뷰 엔티티에 리뷰 이미지 연관관계 추가

--- src/main/java/com/hotsix/server/review/entity/Review.java
+++ src/main/java/com/hotsix/server/review/entity/Review.java
@@ -7,6 +7,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -36,4 +38,18 @@ public class Review extends BaseEntity {
 
     @Column(columnDefinition = "TEXT")
     private String comment;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ReviewImage> reviewImageList = new ArrayList<>();
+
+    public static Review of(Contract contract, User fromUser, User toUser, BigDecimal rating, String comment) {
+        return Review.builder()
+                .contract(contract)
+                .fromUser(fromUser)
+                .toUser(toUser)
+                .rating(rating)
+                .comment(comment)
+                .build();
+    }
 }
