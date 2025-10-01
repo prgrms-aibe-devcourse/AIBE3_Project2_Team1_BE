f7dacc469901e35b2033432c8430c93dd5d8a0fd
haemin4738 <leeheamin12@gmail.com>
2025-10-01 18:15:12 +0900

    [Chore] API 명세서에 맞게 로그인, 로그아웃 Auth로 이동

--- src/main/java/com/hotsix/server/auth/service/AuthService.java
+++ src/main/java/com/hotsix/server/auth/service/AuthService.java
@@ -1,16 +1,23 @@
 package com.hotsix.server.auth.service;
 
+import com.hotsix.server.auth.repository.AuthRepository;
 import com.hotsix.server.global.config.security.jwt.JwtTokenProvider;
+import com.hotsix.server.global.exception.ApplicationException;
 import com.hotsix.server.user.entity.User;
+import com.hotsix.server.user.exception.UserErrorCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final JwtTokenProvider jwtTokenProvider;
+    private final AuthRepository authRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public String genAccessToken(User user) {
         return jwtTokenProvider.generateToken(user.getId(), user.getRole().name());
@@ -24,5 +31,15 @@ public class AuthService {
         Long userId = jwtTokenProvider.getUserId(accessToken);
 
         return Map.of("id", userId);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return authRepository.findByEmail(email);
+    }
+
+    public void checkPassword(User user, String password) {
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new ApplicationException(UserErrorCase.INVALID_PASSWORD);
+        }
     }
 }
