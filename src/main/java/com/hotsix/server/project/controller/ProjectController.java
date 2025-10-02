f5078c20a4a5f2533b95f24b32c15cda40cf74d2
seungin <tmddls91@inha.edu>
2025-10-02 11:18:25 +0900

    [feat] 프로젝트 등록 관련 컨트롤러 로직 구현

--- src/main/java/com/hotsix/server/project/controller/ProjectController.java
+++ src/main/java/com/hotsix/server/project/controller/ProjectController.java
@@ -1,12 +1,37 @@
 package com.hotsix.server.project.controller;
 
 
+import com.hotsix.server.auth.resolver.CurrentUser;
+import com.hotsix.server.global.response.CommonResponse;
+import com.hotsix.server.project.dto.ProjectRequestDto;
+import com.hotsix.server.project.dto.ProjectResponseDto;
+import com.hotsix.server.project.service.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/projects")
 @RequiredArgsConstructor
 public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @PostMapping
+    @Operation(summary = "프로젝트 등록", description = "클라이언트와 프리랜서가 원하는 프로젝트를 등록합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "프로젝트 등록 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "401", description = "인증 실패")
+    })
+    public CommonResponse<ProjectResponseDto> registerProject(
+            @Parameter(hidden = true) @CurrentUser Long userId,
+            @RequestBody @Valid ProjectRequestDto dto
+    ) {
+        return CommonResponse.success(projectService.registerProject(userId, dto));
+    }
 }
