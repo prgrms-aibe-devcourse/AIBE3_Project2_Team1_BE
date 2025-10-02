295764f3b85288122bac66ea8f27d48acfcfaea9
unknown <kimjungho.kr@gmail.com>
2025-10-02 15:37:49 +0900

    [feat] 제안서 테스트 구현

--- src/main/java/com/hotsix/server/proposal/entity/Proposal.java
+++ src/main/java/com/hotsix/server/proposal/entity/Proposal.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor//(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 @Entity
