d1ea039a344ee0a6e780f0785cecee873c06c2b2
Unoguna <kimjungho.kr@gmail.com>
2025-10-02 17:26:13 +0900

    [feat] 프로젝트 관련 메세지 조회 api 구현

--- src/main/java/com/hotsix/server/message/controller/MessageController.java
+++ src/main/java/com/hotsix/server/message/controller/MessageController.java
@@ -1,16 +1,40 @@
 package com.hotsix.server.message.controller;
 
+import com.hotsix.server.global.response.CommonResponse;
+import com.hotsix.server.message.dto.MessageResponseDto;
+import com.hotsix.server.message.entity.Message;
 import com.hotsix.server.message.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@SecurityRequirement(name = "JWT")
 @RequestMapping("/api/v1/messages")
 @RequiredArgsConstructor
+@Tag(name = "MessageController", description = "API 메세지 컨트롤러")
 public class MessageController {
 
     private final MessageService messageService;
+
+    @GetMapping("/{projectId}")
+    @Operation(summary = "프로젝트 관련 메세지 조회")
+    public CommonResponse<List<MessageResponseDto>> getMessagesByProjectId(
+            @PathVariable long projectId
+    ) {
+        List<Message> messages = messageService.findByProjectIdOrderByCreatedAtAsc(projectId);
+
+        return CommonResponse.success(
+                messages.stream().map(MessageResponseDto::new).toList()
+        );
+    }
 
     // 메시지 전송, 조회 등 API 구현
 }
