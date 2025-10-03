ef3d7f7eac84feabd98403452ce1a6bf3f8c261c
seungin <tmddls91@inha.edu>
2025-10-04 00:17:08 +0900

    [feat] 어드민 프로젝트 통계 관련 쿼리 추가

--- src/main/java/com/hotsix/server/project/repository/ProjectRepository.java
+++ src/main/java/com/hotsix/server/project/repository/ProjectRepository.java
@@ -2,6 +2,15 @@ package com.hotsix.server.project.repository;
 
 import com.hotsix.server.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    @Query("SELECT COUNT(p) FROM Project p WHERE DATE(p.createdAt) = :date")
+    long countByCreatedDate(@Param("date") LocalDate date);
+
+    List<Project> findTop10ByOrderByCreatedAtDesc();
 }
