9eaa5269d4b78077ba0cf0516c600830adea4b39
haemin4738 <leeheamin12@gmail.com>
2025-10-05 19:14:47 +0900

    Merge

--- src/main/java/com/hotsix/server/user/repository/UserRepository.java
+++ src/main/java/com/hotsix/server/user/repository/UserRepository.java
@@ -1,11 +1,23 @@
 package com.hotsix.server.user.repository;
 
+import com.hotsix.server.user.entity.Role;
 import com.hotsix.server.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<Object> findByNickname(String nickname);
+
+    @Query("SELECT COUNT(u) FROM User u")
+    long countAllUsers();
+
+    @Query("SELECT COUNT(u) FROM User u WHERE DATE(u.createdAt) = :date")
+    long countUsersByCreatedDate(@Param("date") LocalDate date);
+
+    @Query("SELECT COUNT(u) FROM User u WHERE u.role = :role AND DATE(u.createdAt) = :date")
+    long countByRoleAndCreatedDate(@Param("role") Role role, @Param("date") LocalDate date);
 }
