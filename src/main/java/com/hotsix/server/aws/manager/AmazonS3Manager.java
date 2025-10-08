b7b74cfc95d647e2c47c0eae42ddda9837066e29
seungin <tmddls91@inha.edu>
2025-10-08 17:21:13 +0900

    [feat] S3에 파일 올리고 삭제하기 위한 AmazonS3Manager 구현

---
+++ src/main/java/com/hotsix/server/aws/manager/AmazonS3Manager.java
@@ -0,0 +1,61 @@
+package com.hotsix.server.aws.manager;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AmazonS3Manager {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadFile(MultipartFile file) {
+        String fileName = createFileName(file);
+        String fileUrl = getFileUrl(fileName);
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+
+        try {
+            amazonS3.putObject(bucket, fileName, file.getInputStream(), metadata);
+        } catch (IOException e) {
+            log.error("S3 업로드 실패", e);
+            throw new RuntimeException("S3 업로드 실패", e);
+        }
+
+        return fileUrl;
+    }
+
+    public void deleteFile(String fileUrl) {
+        String key = fileUrl.substring(fileUrl.indexOf(".com/") + 5);
+        amazonS3.deleteObject(bucket, key);
+    }
+
+    private String createFileName(MultipartFile file) {
+        String extension = file.getOriginalFilename()
+                .substring(file.getOriginalFilename().lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString();
+        String datePath = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        return "image/" + datePath + "/" + uuid + extension;
+    }
+
+    private String getFileUrl(String fileName) {
+        return "https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/" + fileName;
+    }
+}
