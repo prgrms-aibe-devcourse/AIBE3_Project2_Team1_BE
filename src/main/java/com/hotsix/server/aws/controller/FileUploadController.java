49bf5cdac04e843b5252c6a32cbe81abd8844e21
seungin <tmddls91@inha.edu>
2025-10-08 17:27:33 +0900

    [refactor] 파일업로드 컨트롤러에 consumes 설정 추가 및 설명 관련 어노테이션 추가

--- src/main/java/com/hotsix/server/aws/controller/FileUploadController.java
+++ src/main/java/com/hotsix/server/aws/controller/FileUploadController.java
@@ -1,6 +1,9 @@
 package com.hotsix.server.aws.controller;
 
 import com.hotsix.server.aws.manager.AmazonS3Manager;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +18,11 @@ public class FileUploadController {
 
     private final AmazonS3Manager s3Manager;
 
-    @PostMapping("/images")
+    @PostMapping(value = "/images", consumes = "multipart/form-data")
+    @Operation(summary = "이미지 업로드", description = "여러 이미지를 업로드합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "업로드 성공")
+    })
     public ResponseEntity<List<String>> uploadImages(@RequestParam("images") List<MultipartFile> files) {
         List<String> urls = files.stream()
                 .map(s3Manager::uploadFile)
@@ -25,6 +32,10 @@ public class FileUploadController {
     }
 
     @DeleteMapping("/images")
+    @Operation(summary = "이미지 업로드", description = "이미지를 삭제합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "이미지 삭제 성공")
+    })
     public ResponseEntity<Void> deleteImage(@RequestParam("url") String imageUrl) {
         s3Manager.deleteFile(imageUrl);
         return ResponseEntity.noContent().build();
