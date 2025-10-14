6b3c988bb1a05f30279fc48d9504dac855ad9288
Unoguna <kimjungho.kr@gmail.com>
2025-10-14 12:53:51 +0900

    [feat] 자기자신과 채팅방 생성 못하게 예외처리

--- src/main/java/com/hotsix/server/message/exception/ChatRoomErrorCase.java
+++ src/main/java/com/hotsix/server/message/exception/ChatRoomErrorCase.java
@@ -10,7 +10,8 @@ public enum ChatRoomErrorCase implements ErrorCase {
 
     CHAT_ROOM_NOT_FOUND(404, 8101, "존재하지 않는 채팅방입니다."),
     ALREADY_JOINED(409, 8102, "이미 참가 중인 채팅방입니다."),
-    FORBIDDEN_ACCESS(403,8103 , "접근할 수 없는 채팅방입니다.");
+    FORBIDDEN_ACCESS(403,8103 , "접근할 수 없는 채팅방입니다."),
+    SELF_CHATROOM_NOT_ALLOWED(400, 8104, "자기 자신과 채팅방을 만들 수 없습니다.");
 
     private final Integer httpStatusCode;
     private final Integer errorCode;
