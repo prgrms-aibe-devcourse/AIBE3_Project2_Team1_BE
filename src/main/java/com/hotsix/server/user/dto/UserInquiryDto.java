00510f762ccf0cfa2411e533347e17edf88612a0
haemin4738 <leeheamin12@gmail.com>
2025-10-14 12:39:51 +0900

    [Feat] 회원 정보 조회 Dto 추가

---
+++ src/main/java/com/hotsix/server/user/dto/UserInquiryDto.java
@@ -0,0 +1,23 @@
+package com.hotsix.server.user.dto;
+
+import com.hotsix.server.user.entity.User;
+
+import java.time.LocalDate;
+
+public record UserInquiryDto(
+        String name,
+        String nickname,
+        String phoneNumber,
+        LocalDate birth,
+        String ProfileImgUrl
+) {
+    public UserInquiryDto(User user){
+        this(
+                user.getName(),
+                user.getNickname(),
+                user.getPhoneNumber(),
+                user.getBirthDate(),
+                user.getPicture()
+        );
+    }
+}
