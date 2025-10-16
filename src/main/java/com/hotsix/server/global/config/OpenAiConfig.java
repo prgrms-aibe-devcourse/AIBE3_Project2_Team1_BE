7090f1c056a8a4a98db5f9bbbf2e5e272f69a8ff
jiyoon-00 <pulip2517@gmail.com>
2025-10-16 12:55:05 +0900

    [fix] OpenAiConfig 설정 되돌리기

--- src/main/java/com/hotsix/server/global/config/OpenAiConfig.java
+++ src/main/java/com/hotsix/server/global/config/OpenAiConfig.java
@@ -1,7 +1,6 @@
 package com.hotsix.server.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -10,12 +9,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class OpenAiConfig {
 
-    @Value("${openai.api-key}")
-    //@Value("${OPENAI_API_KEY}")
+    @Value("${OPENAI_API_KEY}")
     private String apiKey;
 
     @Bean
-    @ConditionalOnProperty(value="openai.api-key")
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getInterceptors().add((request, body, execution) -> {
